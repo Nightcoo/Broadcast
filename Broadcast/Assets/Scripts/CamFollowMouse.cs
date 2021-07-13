a57bddd44f7059d769c033ab595f9328e4f2f11e
Nightcoo <cristianvillaran@ufl.edu>
2021-07-13 19:49:27 -0400

    physical button

---
+++ Broadcast/Assets/Scripts/CamFollowMouse.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CamFollowMouse : MonoBehaviour
+{
+
+    //EXPERIMENTAL AND NON FUNCTIONING
+    public Vector3 mousePos;
+    public int xMaxRot = 10;
+    public int yMaxRot= 14;
+
+    void Update()
+    {
+        mousePos = Input.mousePosition - new Vector3(Screen.width /2, Screen.height /2, 0);
+        mousePos = new Vector3(mousePos.x / (Screen.width /2), mousePos.y /(Screen.height /2), 0);
+
+        mousePos = new Vector3 (mousePos.x * xMaxRot, mousePos.y * yMaxRot, 1);
+
+        //Vector3 relativePos = mousePos - transform.position;
+
+        Quaternion lookRot = Quaternion.LookRotation(mousePos, Vector3.up);
+
+        //Quaternion lookRot = Quaternion(mousePos.x, mousePos.y, transform.rotation.z, transform.rotation.w);
+
+        transform.SetPositionAndRotation(transform.position, lookRot);
+
+        //transform.rotation = lookRot;
+    }
+}
---
+++ Broadcast/Assets/Scripts/UIButton.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+//using TMPro;
+
+public class UIButton : MonoBehaviour {
+
+    //public Sprite regular;
+    //public Sprite mouseOver;
+    //public Sprite mouseClicked;
+    //public TextMeshPro buttonText;
+
+    // Use this for initialization
+    void Start () {
+
+    }
+
+    // Update is called once per frame
+    void Update(){
+
+    }
+
+    private void OnMouseDown(){
+        
+        Debug.Log("CLicked");
+    }
+
+    private void OnMouseEnter(){
+
+    }
+
+    private void OnMouseExit(){
+
+    }
+
+    private void OnMouseUpAsButton(){
+
+    }
+}
