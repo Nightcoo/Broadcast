db6eaffaea1e078ddd3ca5dedbc3acaa99c8bf16
Nightcoo <cristianvillaran@ufl.edu>
2021-12-27 12:44:13 -0500

    camera anim update for drawer

--- Broadcast/Assets/Scripts/PlayAnim.cs
+++ Broadcast/Assets/Scripts/PlayAnim.cs
@@ -14,25 +14,26 @@ public class PlayAnim : MonoBehaviour
     
     public void PlayAnimation(){
 
-        // if(which == 0){ 
+        /*if(which == 0){ 
             
-        //     gameObject.GetComponent<Animation>().Play();
-        //     which = 1; 
-        //     Debug.Log("drawer open");
-        //     }
+            gameObject.GetComponent<Animation>().Play();
+            which = 1; 
+            Debug.Log("drawer open");
+            }
 
-        // else if (which == 1){
+        else if (which == 1){
 
-        //     gameObject.GetComponent<Animation>().Rewind();
-        //     // which = 0; 
-        //     Debug.Log("drawer closed");
-        // }
+            gameObject.GetComponent<Animation>().Rewind();
+            // which = 0; 
+            Debug.Log("drawer closed");
+        }*/
 
         switch(which){
 
             case 0:
                
-               anim ["open_drawer"].speed = 1;
+                // anim["open_drawer"].time = 0;
+                anim ["open_drawer"].speed = 1;
                 anim.Play();
                 which = 1; 
                 Debug.Log("drawer open");
@@ -40,6 +41,7 @@ public class PlayAnim : MonoBehaviour
 
             case 1:
 
+                // anim["open_drawer"].time = anim["bridge"].length;
                 anim ["open_drawer"].speed = -1;
                 anim.Play();
                 which = 0; 
